c54d50a993fc111094161471c901bf89b1950b3f
KaptainSportaco <43203165+KaptainSportaco@users.noreply.github.com>
2019-12-09 23:33:50 +0100

    Animator

---
+++ Bags Please/Assets/Scripts/Animators/AnimationCliente.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    CharacterController cc;
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ("")
+        {
+            anim.SetInteger("condition", 1);
+        }
+        else if ("")
+        {
+            anim.SetInteger("condition", 2);
+        }
+        else
+        {
+            anim.SetInteger("condition", 0);
+        }
+    }
+}
