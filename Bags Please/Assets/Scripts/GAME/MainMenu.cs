e0665afebe5a5a29e9495874194bd2d99bf49af0
KaptainSportaco <43203165+KaptainSportaco@users.noreply.github.com>
2019-12-09 19:19:19 +0100

    Adición de sonidos y música

--- Bags Please/Assets/Scripts/GAME/MainMenu.cs
+++ Bags Please/Assets/Scripts/GAME/MainMenu.cs
@@ -7,16 +7,19 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
+        FindObjectOfType<AudioManager>().Play("InicioPartida");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
 
     public void Settings()
     {
+        FindObjectOfType<AudioManager>().Play("BotonEntrada");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Contact()
     {
+        FindObjectOfType<AudioManager>().Play("BotonEntrada");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 }
--- Bags Please/Assets/Scripts/GAME/Settings.cs
+++ Bags Please/Assets/Scripts/GAME/Settings.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Settings : MonoBehaviour
 {
+    public AudioMixer audiomixermusic;
+    public AudioMixer audiomixereffects;
+
     public void ReturnMainMenu()
     {
+        FindObjectOfType<AudioManager>().Play("BotonSalida");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audiomixermusic.SetFloat("MusicVolume", volume);
+        //FindObjectOfType<AudioManager>().setVolumeMusic(volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        audiomixereffects.SetFloat("EffectsVolume", volume);
+        //FindObjectOfType<AudioManager>().setVolumeEffects(volume);
     }
 }
